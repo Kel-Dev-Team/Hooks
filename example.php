3b64b780726d98c29422e898bcc6ca23b1d2dfdb
Russell Japheth <japheth.russell.i@gmail.com>
2018-01-31 10:51:45 +0100

    expanded the examples

--- example.php
+++ example.php
@@ -37,11 +37,19 @@ require_once __DIR__.'/vendor/autoload.php';
 use Kel\Load;
 use Kel\Hooks;
 
-// ;
+//create a new Hooks instance 
+//passing an array of config options is optional
 $hook = new  Hooks(Load::config(__DIR__.'/config.php'));
-$hook->add_hook('init', "init.php");
-$hook->add_hook('init', "init.php");
-$hook->add_hook('init', "init.php");
+
+//add a hook init.php to the hook point init
+//the hook point is created if it does not exist 
+$hook->add_hook('init', "init.php"); 
+
+//add a hook init.php to the hook point final
 $hook->add_hook('final', "init.php");
+
+//run the hook init
 $hook->run_hook('init');
+
+//run the hook final
 $hook->run_hook('final');
